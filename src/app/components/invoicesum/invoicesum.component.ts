046e7e5bd2bddee535067004cfab94f35cd34497
Ittahad Uz Zaman <ittahad.zaman@selise.ch>
2023-09-09 01:27:27 +0600

    Price receipt added

--- src/app/components/invoicesum/invoicesum.component.ts
+++ src/app/components/invoicesum/invoicesum.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faS, faPrint, faSave } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-invoicesum',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./invoicesum.component.css']
 })
 export class InvoicesumComponent {
-
+  faPrintIcon = faPrint;
+  faSaveIcon = faSave;
 }
--- src/app/components/showproducts/showproducts.component.ts
+++ src/app/components/showproducts/showproducts.component.ts
@@ -17,17 +17,6 @@ export class ShowproductsComponent implements OnInit {
     {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
     {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
     {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
   ];
 
   constructor() { }
